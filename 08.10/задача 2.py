17045a4087828ecbe39e24620a075dfdea0189be
student <boba@mail.ru>
2025-10-08 13:26:29 +0500

    задача 2

---
+++ 08.10/задача 2.py
@@ -0,0 +1,24 @@
+str_in = input()
+
+glas = "аеёиоуыэюя"
+soglas = "бвгджзйклмнпрстфхцчшщ"
+
+c_glas, c_soglas = 0, 0
+letters_freq = {x : 0 for x in glas+soglas}
+
+for e in str_in:
+    if e in glas: 
+        c_glas += 1
+        letters_freq[e] = letters_freq[e] + 1
+    elif e in soglas: 
+        c_soglas += 1
+        letters_freq[e] = letters_freq[e] + 1
+        
+    
+print(f"""Количество согласных: {c_soglas}
+Количество гласных: {c_glas}
+Количество пробелов: {str_in.count(' ')}
+Самые частые символы: {sorted(list(letters_freq.items()), key= lambda x: x[1])[-3:][::-1]}
+Количество слов: {len(str_in.split())}""")
+
+print()
